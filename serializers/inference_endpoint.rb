fb9253f2d583adedf261fc6d0770b628006123a2
Oluwamayowa Oladosu <ceo@layerrail.com>
2026-06-27 20:39:01 +0000

    Keep inference endpoint catalog in sync with Workers AI playground

--- serializers/inference_endpoint.rb
+++ serializers/inference_endpoint.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+﻿# frozen_string_literal: true
 
 class Serializers::InferenceEndpoint < Serializers::Base
   def self.serialize_internal(ie, options = {})
@@ -8,7 +8,17 @@ class Serializers::InferenceEndpoint < Serializers::Base
       display_name: ie.tags["display_name"] || ie.model_name,
       url: ie.load_balancer.health_check_url,
       model_name: ie.model_name,
-      tags: ie.tags.slice("hf_model", "capability", "multimodal", "context_length", "provider"),
+      tags: ie.tags.slice(
+        "api",
+        "capability",
+        "context_length",
+        "deprecated",
+        "display_name",
+        "hf_model",
+        "multimodal",
+        "provider",
+        "source",
+      ),
       price: {
         per_million_prompt_tokens: BillingRate.million_token_price(ie.prompt_billing_resource),
         per_million_completion_tokens: BillingRate.million_token_price(ie.completion_billing_resource),
